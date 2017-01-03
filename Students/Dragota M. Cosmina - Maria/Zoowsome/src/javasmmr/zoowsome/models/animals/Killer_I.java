fba6728f107ca626a608ba3b45a5166ea517526f
Cosmina Dragota <cosminadr09@gmail.com>
2017-01-03 12:09:37 +0200

    Mi-am reinstalat windows-ul si am urmat din nou pasii descrisi in fisierul GitTutorial.pdf

--- Students/Dragota M. Cosmina - Maria/Zoowsome/src/javasmmr/zoowsome/models/animals/Killer_I.java
+++
@@ -1,5 +0,0 @@
-package javasmmr.zoowsome.models.animals;
-
-public interface Killer_I {
-	public boolean kill();
-}
--- Students/Dragota M. Cosmina - Maria/Zoowsome/src/javasmmr/zoowsome/services/factories/EmployeeFactory.java
+++
@@ -1,9 +0,0 @@
-package javasmmr.zoowsome.services.factories;
-import javasmmr.zoowsome.models.employees.*;
-
-public abstract class EmployeeFactory {
-	public abstract Employee getEmployee(String type)
-			throws Exception;
-	// for each type of employee, both add specific entries in the Constants
-	// class and create a factory for it.
-}
